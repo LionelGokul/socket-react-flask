c13b99bc9c6035239e8a3b3ca50d52c00be71960
LionelGokul <72962122+LionelGokul@users.noreply.github.com>
2020-10-25 14:15:42 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO, send
+from configSocket import  config
 
 # initializing APP
 app = Flask("sample-socket")
@@ -9,17 +10,11 @@ socketIO = SocketIO(app, cors_allowed_origins="*")
 
 app.debug = True
 
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-
-@socketIO.on('message', namespace='/api')
-def handleMessage(msg):
-    send(msg, broadcast=True)
-    return None
-
+config(socketIO)
 
 if __name__ == '__main__':
     socketIO.run(app)
